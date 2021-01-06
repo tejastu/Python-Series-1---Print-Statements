1264c336d0c19b73279e74cab73a136778ccca3d
tejastu <tejasparab100@gmail.com>
2021-01-06 20:29:07 +0530

    'print'

---
+++ 1_hello.py
@@ -0,0 +1,10 @@
+# Single line print
+print("Hello world")
+
+
+# Multi line  print
+print('''Lorem ipsum, or lipsum as it is sometimes known,
+ is dummy text used in laying out print, graphic or web designs.
+  The passage is attributed to an unknown typesetter in the 15th century ,
+  who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum
+   for use in a type specimen book.''')
